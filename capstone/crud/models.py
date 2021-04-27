8b9c2c4c7c3fad2e889408fdb90e8fd6871d853d
ritavdas <ritavdas@gmail.com>
2021-04-28 02:17:20 +0530

    Project Completed - 2

---
+++ capstone/crud/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+import datetime
+# Create your models here.
+
+
+class Donors(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.TextField(max_length=1000)
+    contact_number = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    group = models.CharField(max_length=20)
+    weight = models.FloatField()
+    fainting = models.BooleanField(default=False)
+    bruise = models.BooleanField(default=False)
+    vein = models.BooleanField(default=False)
+    dob = models.DateField(default=datetime.date.today)
+    total = models.BooleanField()
+    pic = models.ImageField(null=True, blank=True, upload_to="images/")
+
+    class Meta:
+        db_table = 'blood_donor'
